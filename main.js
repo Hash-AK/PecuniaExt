291825333927f2466fea7c76fd5c7c6543a67f82
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-05 17:34:47 -0400

    added comments for clarity because people might read the code

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function onCreated() {
         console.log("Item created with success");
     }
 }
+// create the different context submenus
 browser.contextMenus.create(
     {
         id: "webpage-value-selection",
@@ -40,21 +41,25 @@ browser.contextMenus.create(
 );
 
 
-
+// listener for contetx menu click
 browser.contextMenus.onClicked.addListener((info, tab) => {
     let regex;
     let match;
     switch (info.menuItemId){
         case "webpage-currency-and-value-selection":
             console.log(info.selectionText);
+            // regex to catch number folowed by any whitespace (or none) followed by 3 letters OR 3 nubmers followed by any whitespaces (or none) followed by numbers
             regex = /([0-9.,]+)\s*([a-zA-Z]{3})|([a-zA-Z]{3})\s*([0-9.,]+)/;
             match = info.selectionText.match(regex);
+            // test if the regex was found
 
             if (match){
+                //get the correct value for each
                 const amountReged = match[1] || match[4];
                 const currencyStr = match[2] || match[3];
                 const amount = amountReged.replace(/,/g, '');
                 let toStore = {
+                    // store the keys in the browser.local.storage
                     context_value: amount,
                     context_currency_input: currencyStr.toUpperCase(),  
                     };
--- popup/menu.js
+++ popup/menu.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", async function() {
+    // setup the necessary variable, html objects for the field and context menu
     const inputCurrencyValueElement = document.getElementById("input-currency-value");
     let inputValue
     const inputCurrencyElement = document.getElementById("input-currency-select");
@@ -8,6 +9,8 @@ document.addEventListener("DOMContentLoaded", async function() {
     let rate
     //let valueFromLocalStorageObj = await browser.storage.local.get("value");
     //let currencyFromLocalStorageObj = await browser.storage.local.get("currency");
+
+    // fetch the data from local storage (if present) and setup necessary step to verify if it need to fillout the fields
     const storedData = await browser.storage.local.get(["context_value", "context_currency_input", "context_currency_output"]);
     let dataFound = false;
     let valueFromLocalStorageData = storedData.context_value;
@@ -33,6 +36,7 @@ document.addEventListener("DOMContentLoaded", async function() {
       //updateConversion();
       dataFound = true;
     }
+    // delete the keys from the storage, if needed
     if (dataFound) {
       await updateConversion();
       browser.storage.local.remove([
@@ -43,13 +47,18 @@ document.addEventListener("DOMContentLoaded", async function() {
     }
 
     async function updateConversion(){
+      // fetch the correct values
       inputValue = inputCurrencyValueElement.value;
       inputCurrency = inputCurrencyElement.value;
       outputCurrency = outputCurrencyElement.value;
+      // manage the api call using the API_KEY variable from popup/config.js
       const response = await fetch('https://v6.exchangerate-api.com/v6/' + API_KEY + '/latest/' + inputCurrency)
+      // parse the json from the api call
       const data = await response.json();
       rate = data.conversion_rates[outputCurrency];
+      // replace the output <p> tag of the popup with the calculaed value
       document.getElementById("output-currency-value").innerHTML = inputValue * rate
+      // debugging lines
       console.log('input value : ' + inputValue);
       console.log('input currency : ' + inputCurrency);
       console.log('output currency : ' + outputCurrency);
@@ -58,6 +67,7 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 
     }
+    // hook listeners to run the updateConversion() function when a value is entered/a field is changed
     inputCurrencyValueElement.addEventListener("input", updateConversion);
     inputCurrencyElement.addEventListener("change", updateConversion)
     outputCurrencyElement.addEventListener("change", updateConversion)
